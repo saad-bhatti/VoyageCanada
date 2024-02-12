21dc43d6764c84401848ac0b5d8cd10272289cd9
saad-bhatti <saad.bhatti.cs@gmail.com>
2024-02-12 12:25:53 -0500

    Add credit for logo image

--- frontend/src/utils/creditsData.js
+++ frontend/src/utils/creditsData.js
@@ -7,4 +7,8 @@ export default [
     source: "https://mui.com/material-ui/react-tabs/",
     use: "Within the custom tabs and custom tab panels seen throughout the client application.",
   },
+  {
+    source: "https://deepai.org/machine-learning-model/abstract-painting-generator",
+    use: "Used to generate the logo for this client application.",
+  },
 ];
