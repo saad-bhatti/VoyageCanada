83ae4052e99fd528b3613af35eaba924eccfcfd0
saad-bhatti <saad.bhatti.cs@gmail.com>
2024-02-13 03:42:59 -0500

    Functioning connection to Postgres server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,37 @@
 import "dotenv/config";
-import mongoose from "mongoose";
+import pg from "pg";
 import app from "./app.js";
 import env from "./utils/validateEnv.js";
 
-// Attempt to connect to MongoDB
-mongoose
-  .connect(env.DATABASE_URL)
-  .then(() => {
+// Create client for PostgreSQL database
+const client = new pg.Client({
+  user: "root",
+  host: "database",
+  database: "voyage_canada",
+  password: "password",
+  port: 5432,
+});
+
+// Connect to the PostgreSQL database
+let retries = 5;
+while (retries) {
+  try {
+    // Connect to the PostgreSQL database
+    await client.connect();
+    console.log("Connected to PostgreSQL database");
+
     // Start the server
-    app.listen(env.PORT, () => {
-      // Log the server port
-      console.log(`Server listening at http://localhost:${env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    // Display error to console
-    console.log(err);
-  });
+    await app.listen(env.PORT);
+    console.log(`Server is running on port ${env.PORT}`);
+
+    break;
+  } catch (err) {
+    // Output necessary information to the console
+    retries -= 1;
+    console.error("Connection error", err);
+    console.log(`Retries left: ${retries}`);
+
+    // Wait for 5 seconds before trying to connect again
+    await new Promise((res) => setTimeout(res, 5000));
+  }
+}
